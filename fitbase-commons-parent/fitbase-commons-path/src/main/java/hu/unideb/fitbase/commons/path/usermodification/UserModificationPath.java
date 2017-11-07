37e7527f4279ebc5c1cf8d5b40f6cf2fa2b50440
Bak Balázs <bak.balazs95@gmail.com>
2017-11-08 00:16:19 +0100

    modification

---
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/usermodification/UserModificationPath.java
@@ -0,0 +1,9 @@
+package hu.unideb.fitbase.commons.path.usermodification;
+
+public class UserModificationPath {
+
+    public static final String USER_MODIFICATION_URL = "/modification";
+
+    private UserModificationPath() {
+    }
+}
