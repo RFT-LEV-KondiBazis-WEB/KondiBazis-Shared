52eeda744fd2b509cf0f81ca464c2e845236b892
Bak Balázs <bak.balazs95@gmail.com>
2017-11-09 23:15:55 +0100

    request refactored

--- fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/request/RegistrationRequest.java
+++ fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/request/RegistrationRequest.java
@@ -1,7 +1,10 @@
 package hu.unideb.fitbase.commons.pojo.request;
 
 import hu.unideb.fitbase.commons.pojo.enumeration.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -10,7 +13,10 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class RegistrationRequest implements Serializable {
 
     private String username;
@@ -27,96 +33,7 @@ public class RegistrationRequest implements Serializable {
 
     private String rememberToken;
 
-    private boolean enabled;
-
-    private Date lastPasswordResetDate;
-
     private LocalDate createdDate;
 
     private UserRole userRole;
-
-    @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
-    }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-
-    public static RegistrationRequest.RegistrationRequestBuilder builder() {
-        return new RegistrationRequest.RegistrationRequestBuilder();
-    }
-
-
-    public static final class RegistrationRequestBuilder {
-        private RegistrationRequest registrationRequest;
-
-        private RegistrationRequestBuilder() {
-            registrationRequest = new RegistrationRequest();
-        }
-
-        public static RegistrationRequestBuilder aRegistrationRequest() {
-            return new RegistrationRequestBuilder();
-        }
-
-        public RegistrationRequestBuilder username(String username) {
-            registrationRequest.setUsername(username);
-            return this;
-        }
-
-        public RegistrationRequestBuilder email(String email) {
-            registrationRequest.setEmail(email);
-            return this;
-        }
-
-        public RegistrationRequestBuilder password(String password) {
-            registrationRequest.setPassword(password);
-            return this;
-        }
-
-        public RegistrationRequestBuilder firstName(String firstName) {
-            registrationRequest.setFirstName(firstName);
-            return this;
-        }
-
-        public RegistrationRequestBuilder lastName(String lastName) {
-            registrationRequest.setLastName(lastName);
-            return this;
-        }
-
-        public RegistrationRequestBuilder rememberToken(String rememberToken) {
-            registrationRequest.setRememberToken(rememberToken);
-            return this;
-        }
-
-        public RegistrationRequestBuilder lastPasswordResetDate(Date lastPasswordResetDate) {
-            registrationRequest.setLastPasswordResetDate(lastPasswordResetDate);
-            return this;
-        }
-
-        public RegistrationRequestBuilder createdDate(LocalDate createdDate) {
-            registrationRequest.setCreatedDate(createdDate);
-            return this;
-        }
-
-        public RegistrationRequestBuilder userRole(UserRole userRole) {
-            registrationRequest.setUserRole(userRole);
-            return this;
-        }
-
-        public RegistrationRequestBuilder but() {
-            return aRegistrationRequest().username(registrationRequest.getUsername()).email(registrationRequest.getEmail()).password(registrationRequest.getPassword()).firstName(registrationRequest.getFirstName()).lastName(registrationRequest.getLastName()).rememberToken(registrationRequest.getRememberToken()).lastPasswordResetDate(registrationRequest.getLastPasswordResetDate()).createdDate(registrationRequest.getCreatedDate()).userRole(registrationRequest.getUserRole());
-        }
-
-        public RegistrationRequest build() {
-            return registrationRequest;
-        }
-    }
 }
--- fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/request/UserModificationRequest.java
+++ fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/request/UserModificationRequest.java
@@ -1,13 +1,17 @@
 package hu.unideb.fitbase.commons.pojo.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class UserModificationRequest {
+@Builder
+public class UserModificationRequest implements Serializable{
     private String email;
 
     private String password;
@@ -17,62 +21,4 @@ public class UserModificationRequest {
     private String firstName;
 
     private String lastName;
-
-    public static UserModificationRequest.UserModificationRequestBuilder builder() {
-        return new UserModificationRequest.UserModificationRequestBuilder();
-    }
-
-    public static final class UserModificationRequestBuilder {
-        private String email;
-        private String password;
-        private String passwordConfirm;
-        private String firstName;
-        private String lastName;
-
-        private UserModificationRequestBuilder() {
-        }
-
-        public static UserModificationRequestBuilder anUserModificationRequest() {
-            return new UserModificationRequestBuilder();
-        }
-
-        public UserModificationRequestBuilder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserModificationRequestBuilder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public UserModificationRequestBuilder passwordConfirm(String passwordConfirm) {
-            this.passwordConfirm = passwordConfirm;
-            return this;
-        }
-
-        public UserModificationRequestBuilder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public UserModificationRequestBuilder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public UserModificationRequestBuilder but() {
-            return anUserModificationRequest().email(email).password(password).passwordConfirm(passwordConfirm).firstName(firstName).lastName(lastName);
-        }
-
-        public UserModificationRequest build() {
-            UserModificationRequest userModificationRequest = new UserModificationRequest();
-            userModificationRequest.setEmail(email);
-            userModificationRequest.setPassword(password);
-            userModificationRequest.setPasswordConfirm(passwordConfirm);
-            userModificationRequest.setFirstName(firstName);
-            userModificationRequest.setLastName(lastName);
-            return userModificationRequest;
-        }
-    }
 }
