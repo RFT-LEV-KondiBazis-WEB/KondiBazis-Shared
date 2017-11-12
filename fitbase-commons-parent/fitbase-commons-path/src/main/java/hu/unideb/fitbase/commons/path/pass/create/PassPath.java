9ee1f1e8b252e9616fd5c5219518bc700481617a
Bak Balázs <bak.balazs95@gmail.com>
2017-11-12 23:45:21 +0100

    pass add

--- fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/pass/create/PassPath.java
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/pass/create/PassPath.java
@@ -2,11 +2,11 @@ package hu.unideb.fitbase.commons.path.pass.create;
 
 public class PassPath {
 
-    public static final String PASS_CREATE_URL = "/pass/create";
+    public static final String PASS_CREATE_URL = "/passes/create";
 
-    public static final String PASS_MODIFICATION_URL = "/pass/modification";
+    public static final String PASS_MODIFICATION_URL = "/passes/update";
 
-    public static final String PASS_DELETE_URL = "/pass/delete";
+//    public static final String PASS_DELETE_URL = "/passes/delete/id";
 
     private PassPath() {
     }
--- fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/usermodification/UserModificationPath.java
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/usermodification/UserModificationPath.java
@@ -2,7 +2,7 @@ package hu.unideb.fitbase.commons.path.usermodification;
 
 public class UserModificationPath {
 
-    public static final String USER_MODIFICATION_URL = "/modification";
+    public static final String USER_MODIFICATION_URL = "/user/update";
 
     private UserModificationPath() {
     }
