9b3241e86757ea8e91a777e278c51d0b81911d9b
Bak Balázs <bak.balazs95@gmail.com>
2017-11-12 18:01:48 +0100

    pass path and pass request add

---
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/pass/create/PassPath.java
@@ -0,0 +1,14 @@
+package hu.unideb.fitbase.commons.path.pass.create;
+
+public class PassPath {
+
+    public static final String PASS_CREATE_URL = "/pass/create";
+
+    public static final String PASS_MODIFICATION_URL = "/pass/modification";
+
+    public static final String PASS_DELETE_URL = "/pass/delete";
+
+    private PassPath() {
+    }
+
+}
