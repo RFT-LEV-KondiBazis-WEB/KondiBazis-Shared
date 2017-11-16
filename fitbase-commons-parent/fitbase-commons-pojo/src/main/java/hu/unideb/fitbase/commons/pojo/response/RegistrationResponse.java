1d07dfbd2e13ef8afe54b7f13db3d0843762ce4f
Bak Balázs <bak.balazs95@gmail.com>
2017-11-16 01:07:53 +0100

    registration response

--- fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/response/RegistrationResponse.java
+++ fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/response/RegistrationResponse.java
@@ -13,48 +13,4 @@ public class RegistrationResponse implements Serializable {
     public void setErrors(Error errors) {
         this.errors = errors;
     }
-
-    //    private String message;
-//
-//    private List<Violation> errors;
-//
-//    public RegistrationResponse() {
-//    }
-//
-//    public RegistrationResponse(String message, List<Violation> errors) {
-//        this.message = message;
-//        this.errors = errors;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public List<Violation> getErrors() {
-//        return errors;
-//    }
-//
-//    public void setErrors(List<Violation> errors) {
-//        this.errors = errors;
-//    }
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        return EqualsBuilder.reflectionEquals(this, obj);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return ToStringBuilder.reflectionToString(this);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return HashCodeBuilder.reflectionHashCode(this);
-//    }
-//
 }
