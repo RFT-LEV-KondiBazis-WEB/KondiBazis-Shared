4e78c51c4550edb3aa5ddf85e482ca785d1604e4
Bak Balázs <bak.balazs95@gmail.com>
2017-12-08 00:38:53 +0100

    path

--- fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/container/PathContainer.java
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/container/PathContainer.java
@@ -6,12 +6,12 @@ public class PathContainer {
 
     public static final String PARAM_PASS_ID = "passId";
 
-    public static final String PARAM_CUSTOMER_ID = "customerId";
+    public static final String PARAM_CUST_ID = "customerId";
 
     public static final String GYM_ID = "/{" + PARAM_GYM_ID + "}";
 
     public static final String PASS_ID = "/{" + PARAM_PASS_ID + "}";
 
-    public static final String CUSTOMER_ID = "/{" + PARAM_CUSTOMER_ID + "}";
+    public static final String CUST_ID= "/{" + PARAM_CUST_ID + "}";
 
 }
---
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/user/UserPath.java
@@ -0,0 +1,12 @@
+package hu.unideb.fitbase.commons.path.user;
+
+public class UserPath {
+
+    /**
+     * URI for user info.
+     */
+    public static final String USERS= "/users";
+
+    private UserPath(){
+    }
+}
