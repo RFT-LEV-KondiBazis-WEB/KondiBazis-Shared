809dd7dd650fde4fd611a4e68b4fef3152f8affe
Bak Balázs <bak.balazs95@gmail.com>
2017-12-08 00:25:19 +0100

    master merge

---
+++ fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/response/GymResponse.java
@@ -0,0 +1,15 @@
+package hu.unideb.fitbase.commons.pojo.response;
+
+public class GymResponse {
+	
+	private Error errors;
+
+	public Error getErrors() {
+		return errors;
+	}
+
+	public void setErrors(Error errors) {
+		this.errors = errors;
+	}
+
+}
