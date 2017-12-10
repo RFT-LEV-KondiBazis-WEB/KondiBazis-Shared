bb42f31949e5d2f03acf9ecbaf667cdd06b0d5d7
Pásztor Gábor <gpasztor87@gmail.com>
2017-12-10 08:44:08 +0100

    Added missing path classes.

---
+++ fitbase-commons-parent/fitbase-commons-path/src/main/java/hu/unideb/fitbase/commons/path/usermodification/UserModificationPath.java
@@ -0,0 +1,12 @@
+package hu.unideb.fitbase.commons.path.usermodification;
+
+public class UserModificationPath {
+
+    /**
+     * URI for user modification.
+     */
+    public static final String USER_MODIFICATION_URL = "/user/update";
+
+    private UserModificationPath() {
+    }
+}
