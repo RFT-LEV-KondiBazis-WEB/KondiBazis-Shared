1d3177db3db9d37fbe335be9287b159bf0cbb014
Bak Balázs <bak.balazs95@gmail.com>
2017-12-14 20:33:49 +0100

    CustomerHistoryRequest.java add

---
+++ fitbase-commons-parent/fitbase-commons-pojo/src/main/java/hu/unideb/fitbase/commons/pojo/request/CustomerHistoryRequest.java
@@ -0,0 +1,10 @@
+package hu.unideb.fitbase.commons.pojo.request;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CustomerHistoryRequest implements Serializable{
+
+    private Date startDate;
+
+}
